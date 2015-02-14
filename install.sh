1f4728893be3477e70235f8cc4f97e6ab6608875
Timothy Danford <tdanford@gmail.com>
2015-02-14 01:29:06 -0500

    Removed the --recursive clone

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or
 dir="$HOME/Development/tdanford"
 mkdir -p $dir
 cd $dir
-git clone --recursive git://github.com/tdanford/dotfiles.git
+git clone git://github.com/tdanford/dotfiles.git
 cd dotfiles
 sudo bash symlink-dotfiles.sh
